f38d65e90e4ca89e3afabe272b2e9764ffca6dbf
aaronk <>
2025-03-24 10:25:21 +1000

    trying the new smp allocator

--- algorithms/InsertionSort/src/main.zig
+++ algorithms/InsertionSort/src/main.zig
@@ -1,11 +1,23 @@
 const std = @import("std");
 
 pub fn insertionSort(comptime T: type, arr: []T) !void {
-    const n = arr.len;
-    for (1..n) |i| {
+    for (arr[1..], 1..) |_, i| {
         const key = arr[i];
+        var j = i;
+
+        while (j > 0 and arr[j - 1] > key) : (j -= 1) {
+            arr[j] = arr[j - 1];
+        }
+        arr[j] = key;
     }
 }
 
-pub fn main() !void {}
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
 
+    var numbers = [_]i32{ 125, 12, 3, 14 };
+
+    try insertionSort(i32, numbers[0..]);
+
+    try stdout.print("{any}\n", .{numbers});
+}
--- concepts/allocators/SmpAllocator/src/main.zig
+++ concepts/allocators/SmpAllocator/src/main.zig
@@ -1,1 +1,21 @@
 const std = @import("std");
+const ArrayList = std.ArrayList;
+
+pub fn main() !void {
+    var smp = std.heap.SmpAllocator(.{ .cpu_count = 2 });
+    defer _ = smp.deinit();
+    const allocator = smp
+
+    var list = ArrayList(u8).init(allocator);
+    defer list.deinit();
+
+    try list.append('H');
+    try list.append('E');
+    try list.append('L');
+    try list.append('L');
+    try list.append('O');
+
+    try list.appendSlice(" World!");
+
+    std.debug.print("{s}\n", .{list.items});
+}
